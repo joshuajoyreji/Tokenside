cd646006c56a4e744895b6a04b3babcdf36e411a
joshuajoyreji <joshuajoyr@gmail.com>
2022-12-02 21:47:23 +0530

    invest styled

--- src/pages/Invest/invest.js
+++ src/pages/Invest/invest.js
@@ -8,10 +8,23 @@ const Invest = () => {
         <div className='container'>
           <div className='row'>
               <div className='offset-1 col-5'>
-                <h4 className='fori '>For Investors</h4>
+                <h3 className='fori '>For Investors</h3>
+                <h4 className='items'>New Deals</h4>
+                <p>Explore new startups</p>
+                <h4 className='items'>Investing 101</h4>
+                <p>Learn about invensting</p>
+                <h4 className='items'>Need Help</h4>
+                <p>Help center & FAQ</p>
               </div>
               <div className='offset-1 col-5 mp'>
-                <h4>Most popular</h4>
+                <h3>Most popular</h3>
+                <p>most searched companies</p>
+                <div className='startup col-8'>
+                  Mobile app-based platform that rewards and gamifies safe driving behaviour
+                </div>
+                <div className='startup col-8'>
+                  Biotech company aiming to treat cancer by exploding cancer cells
+                </div>
               </div>
           </div>
         </div>

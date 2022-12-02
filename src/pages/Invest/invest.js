670f7e02060a9b09e8d09545fe079aae788a9f78
joshuajoyreji <joshuajoyr@gmail.com>
2022-12-02 20:10:12 +0530

    invest

--- src/pages/Invest/invest.js
+++ src/pages/Invest/invest.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './invest.css';
 
-const invest = () => {
+const Invest = () => {
   return (
     <div>
-      
+        <div className='container'>
+          <div className='row'>
+              <div className='offset-1 col-5'>
+                <h4 className='fori '>For Investors</h4>
+              </div>
+              <div className='offset-1 col-5 mp'>
+                <h4>Most popular</h4>
+              </div>
+          </div>
+        </div>
     </div>
   )
 }
 
-export default invest
+export default Invest;

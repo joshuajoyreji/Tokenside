38d99c42baab24c47ad0a6191a38b7069d5a46ef
Ashwin S Kuamar <ashwnskmr@gmail.com>
2022-12-02 16:16:31 +0530

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
 import './App.css';
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import {Home,Browse,Invest,Signin,Signup} from './pages'; 
+import {Navbar} from './components';
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+return (
+  <div className="App">
+<BrowserRouter>
+	<Navbar />
+	<Routes>
+		<Route path='/' element ={<Home/>} />
+		<Route path='/browse' element={<Browse/>} />
+		<Route path='/invest' element={<Invest/>} />
+		<Route path='/signin' element={<Signin/>} />
+		<Route path='/signup' component={<Signup/>} />
+	</Routes>
+  </BrowserRouter>
+  </div>
+);
 }
 
 export default App;
